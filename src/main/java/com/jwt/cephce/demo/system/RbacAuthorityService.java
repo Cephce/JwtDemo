3cfbdcb92951c7c713781b0b362a90f6646606d9
Cephce <cephce@163.com>
2019-07-16 14:16:33 +0800

    添加角色权限控制

--- src/main/java/com/jwt/cephce/demo/system/RbacAuthorityService.java
+++ src/main/java/com/jwt/cephce/demo/system/RbacAuthorityService.java
@@ -1,12 +1,15 @@
 package com.jwt.cephce.demo.system;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -25,7 +28,27 @@ public class RbacAuthorityService {
         if (userInfo instanceof UserDetails) {
 
             String username = ((UserDetails) userInfo).getUsername();
-
+            /**
+            // 获取角色权限
+            Collection<? extends GrantedAuthority> authorities = ((UserDetails) userInfo).getAuthorities();
+            Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+            //循环角色权限，判断是否具有当前路径的权限
+            for (GrantedAuthority authority : authorities) {
+                if (authority.getAuthority().equals("ROLE_ADMIN")) {
+                    //admin 可以访问的资源（一般数据库里维护，这里模拟数据库中查询）
+                    Set<String> urls = new HashSet();
+                    urls.add("/sys/**");
+                    urls.add("/test/**");
+                    AntPathMatcher antPathMatcher = new AntPathMatcher();
+                    for (String url : urls) {
+                        if (antPathMatcher.match(url, request.getRequestURI())) {
+                            hasPermission = true;
+                            break;
+                        }
+                    }
+                }
+            }
+             */
             //获取资源
             Set<String> urls = new HashSet();
             // 这些 url 都是要登录后才能访问，且其他的 url 都不能访问！
